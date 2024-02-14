0ad1ed56b7b9cf600230dbb015b18c1dbe6d6312
Nick Melekian <nick.melekian@gmail.com>
2024-02-14 14:24:08 -0500

    Initial Commit

---
+++ FullMapGame/FullMapGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullMapGameApp.swift
+//  FullMapGame
+//
+//  Created by Nick Melekian on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FullMapGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
